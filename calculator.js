851a390f21f51b8d61c0999fe6439add406c7e68
Aliel Reyes <alielreyes@gmail.com>
2020-07-22 22:53:28 -0700

    Added calculation logic. Having some issues with the display after I get the result sometimes it appends instead of doing a calculation.

--- calculator.js
+++ calculator.js
@@ -1,50 +1,93 @@
+const DOMstrings = {
+    numbersPad: ".numbers--pad",
+    operationsPad: ".operations--pad",
+    displayInput: ".display-input",
+    clearBtn: ".clear-btn"
+};
 
+let result = null;
+let mathSymbol = null;
 
+const numpadElement = document.querySelector(DOMstrings.numbersPad);
+const operationsElement = document.querySelector(DOMstrings.operationsPad);
+const clearBtn = document.querySelector(DOMstrings.clearBtn);
 
+const numpadBtnsCollection = numpadElement.getElementsByTagName('button');
+const operationsBtnsCollection = operationsElement.getElementsByTagName('button');
 
-//Data Structure
-const calculatorModelController = (function () {
+const numpadBtnsArr = Array.prototype.slice.call(numpadBtnsCollection);
+const operationsArr = Array.prototype.slice.call(operationsBtnsCollection);
 
-})();
-
-const calculatorUIController = (function() {
-    const DOMstrings = {
-        numbersPad: ".numbers--pad",
-        displayInput: ".display-input",
-        clearBtn: ".clear-btn"
-    };
-
-
-    return {
-        getDOMstrings: () => {
-            return DOMstrings;
-        },
-
-        updateDisplay: (num) => {
-            console.log(typeof num);
-            if(typeof num !== 'number') return;
-
-            const displayInputEl = document.querySelector(DOMstrings.displayInput);
-            displayInputEl.value = num;
-        }
+const calculate = (num1, symbol, num2) => {
+    let result = 0;
+    switch(symbol){
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            result = num1;
     }
-    
-    
-})();
 
-const calculatorController = (function(modelCtrl, UICtrl) {
+    return result;
+}
 
-    const numpadElement = document.querySelector(UICtrl.getDOMstrings().numbersPad);
-    
-    const numpadBtnsCollection = numpadElement.getElementsByTagName('button');
-    const numpadBtnsArr = Array.prototype.slice.call(numpadBtnsCollection);
+updateDisplay = (num) => {
+    //Cannot show the following characters =
+    if(Number.isNaN(num)) return;
 
-    numpadBtnsArr.forEach((btn) => {
-        btn.addEventListener('click', (event) => {
-            let num = Number.parseFloat(event.target.textContent);
-            UICtrl.updateDisplay(num);
-        });
+    const displayInputEl = document.querySelector(DOMstrings.displayInput);
+    displayInputEl.value = num;
+}
+
+//Event Listeners
+clearBtn.addEventListener("click", () => {
+    const displayInputEl = document.querySelector(DOMstrings.displayInput);
+    result = null;
+    mathSymbol = null;
+    displayInputEl.value = '';
+});
+
+numpadBtnsArr.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        let num = event.target.textContent;
+        const inputValue = document.querySelector(DOMstrings.displayInput).value;
+
+        if(num === '=') {
+            num = calculate(result, mathSymbol, Number.parseFloat(inputValue));
+            mathSymbol = null;
+            result = null;
+        } else if((inputValue === '.' || inputValue > 0) && !mathSymbol) {
+            num = inputValue+num;
+        }
+
+        updateDisplay(Number.parseFloat(num));
     });
+});
 
-})(calculatorModelController, calculatorUIController);
+operationsArr.forEach((btn => {
+    btn.addEventListener('click', (event) => {
+        let currNum = Number.parseFloat(document.querySelector(DOMstrings.displayInput).value);
+        mathSymbol = event.target.textContent;
+
+        if(!result) {
+            result = currNum;
+        } else {
+            result = calculate(result, mathSymbol, currNum);
+            mathSymbol = null;
+        }
+        updateDisplay(result);
+    });
+}));
+
+
+
 
